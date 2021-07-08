10ea2891b3a46ec4c20dd157666fee69ceffd9db
Pavel Naumovets <pavel.naumovets@mail.ru>
2021-07-09 00:17:35 +0300

    Answer

--- src/main/java/com/epam/rd/autocode/floodfill/FloodLogger.java
+++ src/main/java/com/epam/rd/autocode/floodfill/FloodLogger.java
@@ -2,7 +2,7 @@ package com.epam.rd.autocode.floodfill;
 
 public interface FloodLogger {
     default void log(String floodState){
-        System.out.println(floodState + "\n");
+        System.out.println(floodState);
 
     };
 }
